0e7244963d000af95b6fae0bb8cbdf0b81db13cf
tivmof <tomyivanov06@gmail.com>
2025-01-20 16:16:35 +0000

    feat: Added CV property to cards and methods to get user cards

--- pi-bank-backend/api/BankService.ts
+++ pi-bank-backend/api/BankService.ts
@@ -2,6 +2,7 @@ import { BankAccountRepository as BankAccountDao } from "../gen/pi-bank-backend/
 import { CardRepository as CardDao } from "../gen/pi-bank-backend/dao/card/CardRepository";
 import { TransactionRepository as TransactionDao } from "../gen/pi-bank-backend/dao/transaction/TransactionRepository";
 import { UserRepository as UserDao } from "../gen/pi-bank-backend/dao/user/UserRepository";
+import { CardTypeRepository as CardTypeDao } from "../gen/pi-bank-backend/dao/Settings/CardTypeRepository"
 
 import { Controller, Get, Put, Post, response } from "sdk/http";
 
@@ -13,12 +14,14 @@ class BankService {
     private readonly cardDao;
     private readonly transactionDao;
     private readonly userDao;
+    private readonly cardTypeDao;
 
     constructor() {
         this.bankAccountDao = new BankAccountDao();
         this.cardDao = new CardDao();
         this.transactionDao = new TransactionDao();
         this.userDao = new UserDao();
+        this.cardTypeDao = new CardTypeDao();
     }
 
     @Get("/test")
@@ -28,6 +31,55 @@ class BankService {
         response.setStatus(response.OK);
         return msg;
     }
+
+    @Get("/cards/:userId")
+    public getCards(_: any, ctx: any) {
+        const userId = ctx.pathParameters.userId;
+
+        const user = this.userDao.findById(userId);
+
+        if (!user) {
+            response.setStatus(response.NOT_FOUND);
+            return { message: "User with that ID doesn't exist!" };
+        }
+
+        try {
+            const userBankAccounts = this.bankAccountDao.findAll({
+                $filter: {
+                    equals: { User: userId }
+                },
+            });
+
+            const allCards = this.cardDao.findAll();
+
+            let userCards: any = [];
+
+            userBankAccounts.forEach(bankAccount => {
+                allCards.forEach(card => {
+                    if (card.BankAccount === bankAccount.Id) {
+                        userCards.push(card);
+                    }
+                })
+            })
+
+            const finalizaedUserCards = userCards.map(card => {
+                return {
+                    "CardNumber": card.CardNumber,
+                    "ExpirationDate": card.ExpirationDate,
+                    "CardType": this.cardTypeDao.findById(card.CardType).Name,
+                    "Balance": this.bankAccountDao.findById(card.BankAccount).Amount
+                }
+            })
+
+            response.setStatus(response.OK);
+            return { "UserCards": finalizaedUserCards };
+
+        } catch (e: any) {
+            response.setStatus(response.BAD_REQUEST);
+            return { error: e.message };
+        }
+    }
+
 
     @Post("/userLogin")
     public userLogin(body: any) {
@@ -41,8 +93,8 @@ class BankService {
                 }
             }
 
-            let username = body.Username;
-            let password = body.Password;
+            const username = body.Username;
+            const password = body.Password;
 
             const user = this.userDao.findAll({
                 $filter: {
@@ -58,7 +110,7 @@ class BankService {
             response.setStatus(response.OK);
             return { "UserId": user[0].Id };
 
-        } catch (e) {
+        } catch (e: any) {
             response.setStatus(response.BAD_REQUEST);
             return { error: e.message };
         }
