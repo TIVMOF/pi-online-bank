d56c6a66cae88e028d156d024adade57672c32e8
Tomislav Ivanov <78173711+TIVMOF@users.noreply.github.com>
2025-01-30 22:09:42 +0200

    feat: Removing logs

--- pi-bank-backend/tasks/process-transaction.ts
+++ pi-bank-backend/tasks/process-transaction.ts
@@ -9,38 +9,24 @@ const transactionDao = new TransactionDao();
 const execution = process.getExecutionContext();
 const executionId = execution.getId();
 
-console.log("Execution: ", execution);
-console.log("Execution Id: ", executionId);
-
 const senderId = process.getVariable(executionId, "Sender");
 const receiverId = process.getVariable(executionId, "Receiver");
 const amount = process.getVariable(executionId, "Amount");
 const currencyId = process.getVariable(executionId, "Currency");
-
-console.log("senderId: ", senderId);
-console.log("receiverId: ", receiverId);
-console.log("amount: ", amount);
-console.log("currencyId:  ", currencyId);
 
 const receiver = await bankAccountDao.findById(receiverId);
 if (!receiver) {
     throw new Error(`Bank account with ID ${receiverId} not found!`);
 }
 
-console.log("receiver: ", receiver);
-
 const sender = await bankAccountDao.findById(senderId);
 if (!sender) {
     throw new Error(`Bank account with ID ${senderId} not found!`);
 }
 
-console.log("sender: ", sender);
-
 if (!(typeof amount === "number")) {
     throw new Error(`Amount is not a number! Received type: ${typeof amount}.`);
 }
-
-console.log("amount: ", amount);
 
 if (amount <= 0) {
     throw new Error("Amount must be greater than zero.");
@@ -52,8 +38,6 @@ const body = {
     "Amount": amount,
     "Currency": currencyId,
 };
-
-console.log("body: ", body);
 
 const newTransaction = await transactionDao.create(body);
 if (!newTransaction) {
